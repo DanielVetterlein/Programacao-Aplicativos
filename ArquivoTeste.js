a3fe5533b4d43a604da96c7a6abab2d66309f870
DanielVetterlein <vetterleindaniel@gmail.com>
2025-07-28 09:46:02 -0300

    segundo commit

--- ArquivoTeste.js
+++ ArquivoTeste.js
@@ -1,1 +1,6 @@
-console.log("Olá Mundo!")
+const prompt = require("prompt-sync")();
+
+var nome = prompt("Digite o seu nome: ");
+var idade = prompt("Digite a sua idade: ");
+
+console.log("Eae", nome, "seu sigma, tu tem só ", idade, "aninhos né seu tung tung tung sahurzinho");
