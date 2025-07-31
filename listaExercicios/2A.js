ca8f127182024b2497d67f717466f012531295cf
DVetterlein <vetterleindaniel@gmail.com>
2025-07-31 00:28:15 -0300

    Lista - Exercícios 1A até 2F

--- listaExercicios/2A.js
+++ listaExercicios/2A.js
@@ -7,7 +7,7 @@ let nota3 = Number(prompt("Digite a nota3 do aluno: "));
 let mediaAluno = (nota1 + nota2 + nota3) / 3;
 let mediaEscola = 7;
 
-if(mediaAluno>=mediaEscola){
+if(mediaAluno >= mediaEscola){
     console.log(`Aprovado! (${media}>=${mediaEscola}`)
 }else{
     console.log(`Reprovado (${mediaAluno}<${mediaEscola})`)
