ccd26d26b3d0a1ba35dd81e706fcaae02d860dcc
DVetterlein <vetterleindaniel@gmail.com>
2025-07-30 21:24:22 -0300

    Lista - Exercícios 1A até 2D

--- listaExercicios/2A.js
+++ listaExercicios/2A.js
@@ -1,14 +1,14 @@
 const prompt = require("prompt-sync")();
 
-let nota1 = Number(prompt("Digite a nota1: "));
-let nota2 = Number(prompt("Digite a nota2: "));
-let nota3 = Number(prompt("Digite a nota3: "));
+let nota1 = Number(prompt("Digite a nota1 do aluno: "));
+let nota2 = Number(prompt("Digite a nota2 do aluno: "));
+let nota3 = Number(prompt("Digite a nota3 do aluno: "));
 
-let mediaAluno = (nota1 + nota2 + nota3)/3;
-let mediaAprovacao = 7;
+let mediaAluno = (nota1 + nota2 + nota3) / 3;
+let mediaEscola = 7;
 
-if(mediaAluno>=mediaAprovacao){
-    console.log(`Aprovado! (Média aluno: ${mediaAluno})`);
+if(mediaAluno>=mediaEscola){
+    console.log(`Aprovado! (${media}>=${mediaEscola}`)
 }else{
-    console.log(`Não está aprovado (Média aluno: ${mediaAluno})`);
+    console.log(`Reprovado (${mediaAluno}<${mediaEscola})`)
 }
